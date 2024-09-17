676b92f6558f6186e06e78ae53e8fc6af451d4bd
Ahmed Mohamed Goda <136977138+AhmedGoda811@users.noreply.github.com>
2024-09-17 16:16:32 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,25 +2,44 @@ import streamlit as st
 from PIL import Image
 import io
 import requests
+import re
 
+
+def get_response(file):
+    lines = []
+    url = 'https://ahmedgoda-alpr.hf.space/analyze/'
+    files = {'files': (file.name,image_bytes)}
+    responses = requests.post(url,files= files)
+    
+    for response in responses.json()['results'][0]:
+        if len(response) == 1:   #in case of error
+            _, value = response.popitem()
+            lines.append(value) 
+        else:
+            match = re.search(r"(license plate number \d+)", response['filename'])
+            result = match.group(1)
+            lines.append( result+ '\nArabic Text:\t' + response['arabic_text'] + '\nEnglish Text:\t' + response['english_text'])
+    
+    return lines
+
+    
+#start the app
 st.title("Saudi Car License Plate Recognition")
 
+uploaded_files = st.file_uploader("Choose images...", type=["jpg", "jpeg", "png"], accept_multiple_files=True)
 
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
-
-if uploaded_file is not None:
-    
-    image_bytes = uploaded_file.read()
-    image = Image.open(io.BytesIO(image_bytes))
-    st.image(image, caption='Loading...', use_column_width=True)
-
-    url = 'https://ahmedgoda-alpr.hf.space/analyze/'
-    files = {'files': (uploaded_file.name,image_bytes)}
-    response = requests.post(url,files= files)
-    print(response)
-    english_text = response.json()['results'][0][0]['english_text']
-    arabic_text = response.json()['results'][0][0]['arabic_text']
-    st.text_area("The Recognized license Plate in image","Arabic Text:\t"+ arabic_text + "\nEnglish Text:\t " + english_text)
+if uploaded_files is not None:
+    count = 0
+    for uploaded_file in uploaded_files:
+        count += 1
+        image_bytes = uploaded_file.read()
+        image = Image.open(io.BytesIO(image_bytes))
+        st.image(image, caption='Loading...', use_column_width=True)
+        
+        response = "\n".join(get_response(uploaded_file))
+        num_lines = response.count("\n") + 1
+        st.text_area("The Recognized license Plates in this image: ",response,height=num_lines*30)
+        
     
     
     
